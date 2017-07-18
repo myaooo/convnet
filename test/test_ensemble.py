d87ee676dfba4068e6244dbe4c9d041734c0515e
yaoming.thu@gmail.com <yaoming.thu@gmail.com>
2017-07-18 21:55:54 +0800

    bug fixes from remote

--- test/test_ensemble.py
+++ test/test_ensemble.py
@@ -2,12 +2,14 @@ import tensorflow as tf
 
 from convnet.ensemble import EnsembleModel
 from convnet.core import ConvNet
+from convnet.utils import init_tf_environ
 from convnet.preprocess import prepare_data_fer2013, IMG_SIZE, CHANNELS
 import convnet.model_face as model_pool
 
 def test():
+    init_tf_environ(gpu_num=1)
     # model_list = [model_pool.model0, model_pool.model0]
-    name_list = ['Test2']
+    name_list = ['ResNS128-3', 'ResNS128-2']
     models = [ConvNet(name) for name in name_list]
     graph = tf.Graph()
     model = EnsembleModel(models, [IMG_SIZE[0], IMG_SIZE[1], CHANNELS], name='ensemble', graph=graph)
